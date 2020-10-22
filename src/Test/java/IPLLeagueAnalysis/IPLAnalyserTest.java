3519ec57dd2d1f70d8a868ec9759f92072c25d70
madhu179 <madhubabu232343@gmail.com>
2020-10-22 10:10:36 +0530

    Adding Test Case for Best Economy

--- src/Test/java/IPLLeagueAnalysis/IPLAnalyserTest.java
+++ src/Test/java/IPLLeagueAnalysis/IPLAnalyserTest.java
@@ -104,5 +104,16 @@ public class IPLAnalyserTest {
 			e.printStackTrace();
 		}
 	}
+	
+	@Test
+	public void givenWktsCsvDataShouldReturnBestEconomy() {
+		try {
+			iplAnalyser.loadWktsData(PLAYER_WKTS_DATA);
+			String playerName = iplAnalyser.getBestEconomy();
+			assertEquals("Shivam Dube", playerName);
+		} catch (IPLAnalyserException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
